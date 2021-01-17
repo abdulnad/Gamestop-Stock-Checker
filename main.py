6477200c682b6ea2bd791f48450ad33b7609446c
abdulnad <53980282+abdulnad@users.noreply.github.com>
2021-01-17 15:30:18 -0500

    updated

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
-from the_bot import the_bot
+from stock_checker import stock_checker
 
-bot = the_bot()
-bot.sign_in()
-bot.print_status()
-#bot.check_for_stock()
+checker = stock_checker()
+#stock_checker.sign_in()
+sc = bot.print_status()
+if sc == 200:
+  bot.check_for_stock()
+else:
+  print('Could not reach page: status code {}'.format(sc))
